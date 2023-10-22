4e8d1199b5fee99c4f2fb8f441c02961d7cfa7c3
Alexey Zotov <Alexey.Zotov@ymail.com>
2023-10-22 23:06:03 +0300

    ConsoleIO class updated with PrintDate  method

--- Exersise001/ConsoleIO.cs
+++ Exersise001/ConsoleIO.cs
@@ -1,15 +1,25 @@
 ﻿using System.Reflection;
+using System.Runtime.ConstrainedExecution;
 using System.Runtime.Intrinsics.Arm;
 
 namespace Exersise001;
 
 public abstract class ConsoleIO
 {
-    public static void PrintHello(){
+    public static void PrintHello()
+    {
         Console.WriteLine("Hello");
     }
 
-    public static string GetUserInput(){
+    public static string GetUserInput()
+    {
         return Console.ReadLine();
     }
+
+
+    public static void PrintDate()
+    {
+        System.Console.WriteLine(DateTime.Now);
+    }
 }
+

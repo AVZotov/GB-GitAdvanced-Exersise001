7fd9598ed9f1a9674cf158b643bc44e39d7dc580
Alexey Zotov <Alexey.Zotov@ymail.com>
2023-10-22 22:06:32 +0300

    ConsoleIO class updated with getuserInput method

--- Exersise001/ConsoleIO.cs
+++ Exersise001/ConsoleIO.cs
@@ -1,8 +1,15 @@
-﻿namespace Exersise001;
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Exersise001;
 
 public abstract class ConsoleIO
 {
     public static void PrintHello(){
         Console.WriteLine("Hello");
+    }
+
+    public static string GetUserInput(){
+        return Console.ReadLine();
     }
 }

5236e00131753de9349c7f804091166069b8bbe6
Alexey Zotov <Alexey.Zotov@ymail.com>
2023-10-24 23:15:56 +0300

    removed getDate method from ConsoleIO class

--- Exersise001/ConsoleIO.cs
+++ Exersise001/ConsoleIO.cs
@@ -10,11 +10,5 @@ public abstract class ConsoleIO
     {
         return Console.ReadLine();
     }
-
-
-    public static void PrintDate()
-    {
-        System.Console.WriteLine(DateTime.Now);
-    }
 }
 
